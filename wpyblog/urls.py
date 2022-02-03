249960d437aa719d0db6b781705f984f9bf99dd3
Kamal Mustafa <kamal@xoxzo.com>
2022-02-03 18:37:57 +0900

    fix urls

--- wpyblog/urls.py
+++ wpyblog/urls.py
@@ -18,9 +18,9 @@ app_name = "wpyblog"
 
 urlpatterns = [
     url(r"category/(?P<category_id>\d+)/(?P<slug>[-\w]+)/", category_list_post, name="category-post-list"),
-    url(r"tag/(?P<tag_id>\d+)/(?P<slug>[-\w]+)/", tag_list_post, name="tag-post-list"),
-    url(r"(?P<post_id>\d+)/(?P<slug>[-\w]+)/", view_post, name="view-post"),
-    url(r"preview/(?P<post_id>\d+)/", preview_post, name="preview-post"),
-    url("clear/", clear_cache, name="clear-post"),
+    url(r"^tag/(?P<tag_id>\d+)/(?P<slug>[-\w]+)/", tag_list_post, name="tag-post-list"),
+    url(r"^(?P<post_id>\d+)/(?P<slug>[-\w]+)/", view_post, name="view-post"),
+    url(r"^preview/(?P<post_id>\d+)/", preview_post, name="preview-post"),
+    url("^clear/", clear_cache, name="clear-post"),
     url("", list_post, name="blog"),
 ]

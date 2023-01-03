5008ab557f1e06faf0c90a20077334de878125cd
Josef <josef@xoxzo.com>
2023-01-03 07:24:42 +0000

    20230103 fix other slug pattern

--- wpyblog/urls.py
+++ wpyblog/urls.py
@@ -18,8 +18,8 @@ app_name = "wpyblog"
 
 urlpatterns = [
     url(r"category/(?P<category_id>\d+)/(?P<slug>[-\S]+)/", category_list_post, name="category-post-list"),
-    url(r"^tag/(?P<tag_id>\d+)/(?P<slug>[-\w]+)/", tag_list_post, name="tag-post-list"),
-    url(r"^(?P<post_id>\d+)/(?P<slug>[-\w]+)/", view_post, name="view-post"),
+    url(r"^tag/(?P<tag_id>\d+)/(?P<slug>[-\S]+)/", tag_list_post, name="tag-post-list"),
+    url(r"^(?P<post_id>\d+)/(?P<slug>[-\S]+)/", view_post, name="view-post"),
     url(r"^preview/(?P<post_id>\d+)/", preview_post, name="preview-post"),
     url("^clear/", clear_cache, name="clear-post"),
     url("", list_post, name="blog"),

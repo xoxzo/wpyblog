41dc61b050bc357bf85997e66fd8e73102f983b3
Aji <atamasite@gmail.com>
2023-05-25 11:11:55 +0700

    EZSMS-998 add title_page_generator

--- wpyblog/templatetags/blog_tags.py
+++ wpyblog/templatetags/blog_tags.py
@@ -6,3 +6,18 @@ register = template.Library()
 @register.filter(name="string_to_date")
 def string_to_date(date_string):
     return parse_datetime(date_string).strftime("%B %-d, %Y")
+
+@register.simple_tag(takes_context=True)
+def title_page_generator(context, path):
+    title_page_en = "Low cost & No initial set-up cost. International SMS delivery service EZSMS"
+    title_page_ja = "低価格、低コスト、初期費用なしの国内・国際ショートメッセージSMS配信サービスEZSMS"
+
+    try:
+        if 'en' in path:
+            return title_page_en + " | " + path.split('/')[-2].replace("-", " ").capitalize()
+        return title_page_ja + " | " + path.split('/')[-2].replace("-", " ").capitalize()
+
+    except:
+        if 'en' in path:
+            return title_page_en
+        return title_page_ja
